c846af92e5289604940c4c153d93b69f5f5cabeb
Gilberto Junior <giljrsantos@gmail.com>
2025-04-16 14:12:04 -0300

    feat: adicionar suporte ao ng-zorro-antd e configurar tema personalizado

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,16 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { pt_PT, provideNzI18n } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import pt from '@angular/common/locales/pt';
+import { FormsModule } from '@angular/forms';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideHttpClient } from '@angular/common/http';
+
+registerLocaleData(pt);
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)],
+  providers: [provideRouter(routes), provideNzI18n(pt_PT), importProvidersFrom(FormsModule), provideAnimationsAsync(), provideHttpClient()],
 };
