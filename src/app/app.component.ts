e23c6938ec7e1508d9e44f32a1ad7f902bb1f774
Gilberto Junior <giljrsantos@gmail.com>
2025-04-16 08:26:42 -0300

    feat: migrate testing framework from Karma to Jest and update related configurations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { DynamicFormComponent } from './components/dynamic-form/dynamic-form.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,DynamicFormComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
