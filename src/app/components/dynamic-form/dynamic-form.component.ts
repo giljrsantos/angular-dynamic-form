145d6eb4bd6df6e683a0b2dc89f144fd21fc6a9e
Gilberto Junior <giljrsantos@gmail.com>
2025-04-17 07:07:05 -0300

    feat: adicionar a biblioteca @ctrl/tinycolor e atualizar o título da aplicação

--- src/app/components/dynamic-form/dynamic-form.component.ts
+++ src/app/components/dynamic-form/dynamic-form.component.ts
@@ -1,9 +1,23 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-
+import { ReactiveFormsModule } from '@angular/forms';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzCardModule } from 'ng-zorro-antd/card';
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzSelectModule } from 'ng-zorro-antd/select';
 @Component({
   selector: 'app-dynamic-form',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    NzInputModule,
+    NzButtonModule,
+    NzFormModule,
+    NzSelectModule,
+    NzCardModule
+  ],
   templateUrl: './dynamic-form.component.html',
   styleUrl: './dynamic-form.component.scss',
 })
