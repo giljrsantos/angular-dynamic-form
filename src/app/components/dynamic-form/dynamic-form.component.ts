b93b6d5f4cf9f9be6a933cbd5890cb1202384973
Gilberto Junior <giljrsantos@gmail.com>
2025-04-17 07:24:40 -0300

    feat: implementar componente de formulário dinâmico com validação e mensagens de sucesso/erro

--- src/app/components/dynamic-form/dynamic-form.component.ts
+++ src/app/components/dynamic-form/dynamic-form.component.ts
@@ -1,11 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { NzButtonModule } from 'ng-zorro-antd/button';
 import { NzCardModule } from 'ng-zorro-antd/card';
 import { NzFormModule } from 'ng-zorro-antd/form';
 import { NzInputModule } from 'ng-zorro-antd/input';
 import { NzSelectModule } from 'ng-zorro-antd/select';
+import { NzMessageService } from 'ng-zorro-antd/message';
+import { FORM_FIELDS } from '../../../assets/form-fields/form-fields';
 @Component({
   selector: 'app-dynamic-form',
   standalone: true,
@@ -21,4 +23,30 @@ import { NzSelectModule } from 'ng-zorro-antd/select';
   templateUrl: './dynamic-form.component.html',
   styleUrl: './dynamic-form.component.scss',
 })
-export class DynamicFormComponent {}
+export class DynamicFormComponent {
+  formGroup!: FormGroup;
+  fields = FORM_FIELDS;
+
+  constructor(
+    private fb: FormBuilder,
+    private message: NzMessageService,
+  ){}
+
+  ngOnInit(): void {
+    this.formGroup = this.fb.group({});
+    this.fields.forEach(field => {
+      this.formGroup.addControl(
+        field.key,
+        this.fb.control('', field.required ? Validators.required: [])
+      );
+    });
+  }
+
+  onSubmit(): void {
+    if (this.formGroup.valid) {
+      this.message.success('Formulário enviado com sucesso!');
+    } else {
+      this.message.error('Preencha todos os campos obrigatórios!');
+    }
+  }
+}
